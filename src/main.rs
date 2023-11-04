861909493a4e61bfe88b8de700914d5647f5f92e
Aron Bergman <bathingrad@gmail.com>
2023-11-05 00:00:03 +0100

    Read secrets from file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
 fn main() {
-    println!("Hello, world!");
+    let secrets = {
+        let path = "secrets.toml";
+        let contents = match std::fs::read_to_string(path) {
+            Ok(s) => s,
+            Err(_) => {
+                eprintln!("Couldn't read secrets from {path}.");
+                return;
+            },
+        };
+
+        match contents.parse::<toml::Table>() {
+            Ok(s) => s,
+            Err(_) => {
+                eprintln!("Couldn't read secrets from {path}.");
+                return;
+            },
+        }
+    };
 }
