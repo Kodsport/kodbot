c23b72f8866001a8c6eb6b7ebd48273967e048ec
Aron Bergman <bathingrad@gmail.com>
2023-11-05 21:34:27 +0100

    Seralize configuration from file

---
+++ src/config.rs
@@ -0,0 +1,52 @@
+use twilight_model::id::Id;
+use twilight_model::id::marker::{GuildMarker, ChannelMarker};
+
+use serde::{Serialize, Deserialize};
+
+pub const DEFAULT_PATH: &str = "config.toml";
+
+#[derive(Deserialize, Serialize)]
+pub struct Config {
+	guild: Id<GuildMarker>,
+	welcome: Welcome,
+}
+
+#[derive(Deserialize, Serialize)]
+pub struct Welcome {
+	channel: Id<ChannelMarker>,
+	file: Option<String>,
+	text: Option<String>,
+}
+
+impl Config {
+	pub fn guild(&self) -> &Id<GuildMarker> {
+		&self.guild
+	}
+
+	pub fn welcome(&self) -> &Welcome {
+		&self.welcome
+	}
+}
+
+impl Welcome {
+	pub fn channel(&self) -> Id<ChannelMarker> {
+		self.channel
+	}
+
+	pub fn message(&self) -> Option<String> {
+		// Start by checking the text key, i.e. it will override the file.
+		if let Some(t) = &self.text {
+			return Some(t.clone());
+		}
+
+		if let Some(f) = &self.file {
+			// TODO Throwing away info here by turning it into an Option.
+			let message = std::fs::read_to_string(f).ok();
+			return message;
+		}
+
+		// Neither a file path nor a text was given in the configuration,
+		// so we have no welcome message.
+		None
+	}
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use twilight_http::Client;
-use twilight_model::id::Id;
 
+mod config;
 mod state;
 mod secrets;
 mod welcome;
@@ -10,10 +10,8 @@ async fn main() {
     let s = std::fs::read_to_string(secrets::DEFAULT_PATH).expect("Couldn't read secrets.");
     let secrets: secrets::Secrets = toml::from_str(&s).expect("Couldn't read secrets.");
 
-    let path = "config.toml";
-    let config = std::fs::read_to_string(path).ok()
-        .and_then(|c| c.parse::<toml::Table>().ok())
-        .expect(&format!("Couldn't read configuration form {path}"));
+    let s = std::fs::read_to_string(config::DEFAULT_PATH).expect("Couldn't read configuration.");
+    let config: config::Config = toml::from_str(&s).expect("Couldn't read configuration.");
 
     let mut state = match state::from_file(state::DEFAULT_PATH) {
         Ok(state) => state,
@@ -26,24 +24,17 @@ async fn main() {
     let client = Client::new(String::from(secrets.token));
 
     if state.welcome().is_none() {
-        let channel_id = Id::new(
-            config.get("welcome")
-            .and_then(|w| w.as_table())
-            .and_then(|w| w.get("channel"))
-            .and_then(|c| c.as_integer())
-            .and_then(|c| c.try_into().ok()) // NOTE Discord IDs are u64 and toml parsing can only handle i64, but IDs are currently < i64 max.
-            .expect("Couldn't get channel for welcome message."));
+        let channel = config.welcome().channel();
+        let message = config.welcome().message();
 
-        let message_path = config.get("welcome")
-            .and_then(|w| w.as_table())
-            .and_then(|w| w.get("file"))
-            .and_then(|p| p.as_str())
-            .expect("Couldn't get path for welcome message.");
-        let message = std::fs::read_to_string(message_path).expect(&format!("Couldn't read message from {message_path}."));
-        let message = welcome::post_welcome_message(&client, channel_id, message).await;
-        state.set_welcome(state::Welcome::new(message.id));
-        if let Err(_) = state::to_file(state::DEFAULT_PATH, &state) {
-            eprintln!("Couldn't write state to file!");
+        if let Some(message) = message {
+            let message = welcome::post_welcome_message(&client, channel, message).await;
+            state.set_welcome(state::Welcome::new(message.id));
+            if let Err(_) = state::to_file(state::DEFAULT_PATH, &state) {
+                eprintln!("Couldn't write state to file!");
+            }
+        } else {
+            eprintln!("No welcome message was given in configuration.");
         }
     }
 }
