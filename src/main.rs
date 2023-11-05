89bfdb907a6da271aaed792f56cabe75a98ce518
Aron Bergman <bathingrad@gmail.com>
2023-11-05 13:47:27 +0100

    Read configuration file

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,11 @@ async fn main() {
         .and_then(|c| c.parse::<toml::Table>().ok())
         .expect(&format!("Couldn't read secrets from {path}"));
 
+    let path = "config.toml";
+    let config = std::fs::read_to_string(path).ok()
+        .and_then(|c| c.parse::<toml::Table>().ok())
+        .expect(&format!("Couldn't read configuration form {path}"));
+
     let token = secrets.get("token").and_then(|v| v.as_str()).expect("Couldn't read token from secrets.");
     let client = Client::new(String::from(token));
 }
